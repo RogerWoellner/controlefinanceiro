19e60e22d4661177b829248cb7e64f3233256bf9
RogerWoellner <115326179+RogerWoellner@users.noreply.github.com>
2023-03-19 01:07:30 -0300

    Add files via upload

---
+++ src/components/header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+    height: 150px;
+    text-align: center;
+    background: #27a4f2;
+`;
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+    padding-top: 20px;
+    color: #fff;
+`;
